7589af2bca0708f928b86ec485f661f301889a18
scilo7 <scilo7@users.noreply.github.com>
2017-02-28 09:59:44 +0100

    real unit tests

--- tests/test.py
+++ tests/test.py
@@ -1,37 +1,51 @@
+
+import unittest
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-Session = sessionmaker(bind=create_engine('sqlite:///:memory:', echo=False))
-Base = declarative_base()
-session = Session()
-
 from dynalchemy import Registry
 
-reg = Registry(Base, session)
 
-Bird = reg.add('animal', 'bird', columns=[
-    dict(name='name', kind='string', max=200, null=False),
-    dict(name='nb_wings', kind='integer', null=False),
-    dict(name='color', kind='string', max=200),
-])
+class TestRegistry(unittest.TestCase):
 
-Bird2 = reg.add('animal', 'bird2', columns=[
-    dict(name='name', kind='string', max=200, null=False),
-    dict(name='nb_wings', kind='integer', null=False),
-    dict(name='color', kind='string', max=200),
-])
+    @classmethod
+    def setUpClass(cls):
+        Session = sessionmaker(bind=create_engine('sqlite:///:memory:', echo=False))
+        Base = declarative_base()
+        session = Session()
+        cls.reg = Registry(Base, session)
+
+    def test_add(self):
+        Bird = self.reg.add('animal', 'bird', columns=[
+            dict(name='name', kind='string', max=200, null=False),
+            dict(name='nb_wings', kind='integer', null=False),
+            dict(name='color', kind='string', max=200),
+        ])
+        Bird2 = self.reg.add('animal', 'bird2', columns=[
+            dict(name='name', kind='string', max=200, null=False),
+            dict(name='nb_wings', kind='integer', null=False),
+            dict(name='color', kind='string', max=200),
+        ])
+        self.assertEqual(Bird.__tablename__, 'animal__bird')
+        self.assertEqual(Bird2.__tablename__, 'animal__bird2')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+'''
 
 Bird = reg.get('animal', 'bird')
-print 'all: ', reg.list('animal')
-print 'one: ', Bird
 
 session.add(Bird(name='pinson', nb_wings=4, color='lightblue'))
 session.commit()
 
-print 'birds', session.query(Bird).all()
 pinson = session.query(Bird).filter_by(name='pinson').one()
-print 'pinson color', pinson.color
 
 reg.add_column('animal', 'bird', dict(name='food', kind='string', max=200))
 pinson = session.query(Bird).filter_by(name='pinson').one()
@@ -40,4 +54,4 @@ session.commit()
 
 reg.deprecate_column('animal', 'bird', 'food')
 reg.deprecate('animal', 'bird')
-print reg.get('animal', 'bird')
+reg.get('animal', 'bird')'''
